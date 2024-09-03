31f5f7b11f5e2642b5d078a6cd30e3c3e9777117
jianfeng <gjf610@yeah.net>
2024-09-03 18:51:02 +0800

    feat: render target

--- src/components/game/Game.vue
+++ src/components/game/Game.vue
@@ -1,22 +1,27 @@
 <template>
   <div>
     <Map />
+    <template v-for="target in targets">
+      <Target :x="target.x" :y="target.y" />
+    </template>
+
     <Player />
     <template v-for="cargo in cargos">
       <Cargo :x="cargo.x" :y="cargo.y" />
     </template>
-
   </div>
 </template>
 <script setup lang="ts">
 import Map from './Map.vue'
 import Player from './Player.vue'
 import Cargo from './Cargo.vue';
+import Target from './Target.vue';
 import { useCargoStore } from '../../store/cargo';
-
+import { useTargetStore } from '../../store/tatget';
 
 const { cargos, addCargo, createCargo } = useCargoStore()
 addCargo(createCargo({ x: 2, y: 2 }))
 addCargo(createCargo({ x: 3, y: 3 }))
 
+const { targets } = useTargetStore()
 </script>
---
+++ src/components/game/Target.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="absolute" :style="position">
+    <img :src="targetImg" alt="">
+  </div>
+</template>
+
+<script setup lang="ts">
+import targetImg from '../../assets/target.png'
+import { Position, usePosition } from '../../composables/usePosition'
+
+
+
+const props = defineProps<Position>()
+const { position } = usePosition(props)
+
+
+</script>
