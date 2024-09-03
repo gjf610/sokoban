6fbe67f43a8c47b004c71a40182a3d9b9578b921
jianfeng <gjf610@yeah.net>
2024-09-03 16:15:46 +0800

    feat: player move to wall

--- src/store/tests/map.spec.ts
+++ src/store/tests/map.spec.ts
@@ -1,7 +1,32 @@
-import { it, expect, describe } from 'vitest'
+import { it, expect, describe, beforeEach } from 'vitest'
+import { useMapStore } from '../map'
+import { createPinia, setActivePinia } from 'pinia'
 
 describe('map test', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
   it('should', () => {
-    expect(true).toBe(true)
+    const { map } = useMapStore()
+
+    expect(map).toEqual([
+      [1, 1, 1, 1, 1],
+      [1, 2, 2, 2, 1],
+      [1, 2, 2, 2, 1],
+      [1, 2, 2, 2, 1],
+      [1, 1, 1, 1, 1],
+    ])
+  })
+  it('should setup map', () => {
+    const { map, setupMap } = useMapStore()
+    const newMap = [
+      [1, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1],
+    ]
+
+    setupMap(newMap)
+
+    expect(map).toEqual(newMap)
   })
 })
--- src/store/tests/player.spec.ts
+++ src/store/tests/player.spec.ts
@@ -1,45 +1,107 @@
 import { it, expect, describe, beforeEach } from 'vitest'
 import { usePlayerStore } from '../player'
 import { createPinia, setActivePinia } from 'pinia'
+import { useMapStore } from '../map'
 
 describe('player', () => {
   beforeEach(() => {
     setActivePinia(createPinia())
   })
-  it('should move to left', () => {
-    const { movePlayerToLeft, player } = usePlayerStore()
-    player.x = 1
-    player.y = 1
+  describe('normal move', () => {
+    beforeEach(() => {
+      const { setupMap } = useMapStore()
+      setupMap([
+        [2, 2, 2],
+        [2, 2, 2],
+        [2, 2, 2],
+      ])
+    })
 
-    movePlayerToLeft()
+    it('should move to left', () => {
+      const { movePlayerToLeft, player } = usePlayerStore()
+      player.x = 1
+      player.y = 1
 
-    expect(player.x).toBe(0)
+      movePlayerToLeft()
+
+      expect(player.x).toBe(0)
+    })
+    it('should move to right', () => {
+      const { movePlayerToRight, player } = usePlayerStore()
+      player.x = 1
+      player.y = 1
+
+      movePlayerToRight()
+
+      expect(player.x).toBe(2)
+    })
+    it('should move to up', () => {
+      const { movePlayerToUp, player } = usePlayerStore()
+      player.x = 1
+      player.y = 1
+
+      movePlayerToUp()
+
+      expect(player.y).toBe(0)
+    })
+    it('should move to down', () => {
+      const { movePlayerToDown, player } = usePlayerStore()
+      player.x = 1
+      player.y = 1
+
+      movePlayerToDown()
+
+      expect(player.y).toBe(2)
+    })
   })
-  it('should move to right', () => {
-    const { movePlayerToRight, player } = usePlayerStore()
-    player.x = 1
-    player.y = 1
+  describe('collision wall', () => {
+    beforeEach(() => {
+      let map = [
+        [1, 1, 1, 1, 1],
+        [1, 2, 2, 2, 1],
+        [1, 2, 2, 2, 1],
+        [1, 2, 2, 2, 1],
+        [1, 1, 1, 1, 1],
+      ]
+      const { setupMap } = useMapStore()
+      setupMap(map)
+    })
+    it('should not move to left when collision a wall', () => {
+      const { movePlayerToLeft, player } = usePlayerStore()
+      player.x = 1
+      player.y = 1
 
-    movePlayerToRight()
+      movePlayerToLeft()
 
-    expect(player.x).toBe(2)
+      expect(player.x).toBe(1)
+    })
+    it('should not move to right when collision a wall', () => {
+      const { movePlayerToRight, player } = usePlayerStore()
+      player.x = 3
+      player.y = 1
+
+      movePlayerToRight()
+
+      expect(player.x).toBe(3)
+    })
+    it('should not move to up when collision a wall', () => {
+      const { movePlayerToUp, player } = usePlayerStore()
+      player.x = 1
+      player.y = 1
+
+      movePlayerToUp()
+
+      expect(player.y).toBe(1)
+    })
+    it('should not move to down when collision a wall', () => {
+      const { movePlayerToDown, player } = usePlayerStore()
+      player.x = 1
+      player.y = 3
+
+      movePlayerToDown()
+
+      expect(player.y).toBe(3)
+    })
   })
-  it('should move to up', () => {
-    const { movePlayerToUp, player } = usePlayerStore()
-    player.x = 1
-    player.y = 1
 
-    movePlayerToUp()
-
-    expect(player.y).toBe(0)
-  })
-  it('should move to down', () => {
-    const { movePlayerToDown, player } = usePlayerStore()
-    player.x = 1
-    player.y = 1
-
-    movePlayerToDown()
-
-    expect(player.y).toBe(2)
-  })
 })
